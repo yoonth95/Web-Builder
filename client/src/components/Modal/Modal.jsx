b25161097e7674c29bd78695c7ff603f8bedae65
shinjeoungho <hole546@naver.com>
2023-07-21 12:39:27 +0900

    :sparkles: Feat inputValue hook 추가

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-// import { setBtn } from 'redux/buttonSlice';
+import useInputValues from 'hooks/useInput'
 import { updateFirstList, updateSecondList } from 'redux/menuSlice';
 import 'styles/Modal/Modal.css';
 
 const Modal = ({ isOpen, setIsOpen }) => {
   const { btn } = useSelector((state) => state.btn);
   const { firstList, secondList } = useSelector((state) => state.menu);
+  const { inputValues, handleChange, reset}= useInputValues({
+    title:"",
+    link:"",
+    new_window: 0
+  })
+  const {title,link,new_window} = inputValues
 
-  const [title, setTitle] = useState('');
-  const [link, setLink] = useState('');
-  const [window, setWindow] = useState(0);
   const parent_id = btn ? Number(btn.slice(2)) : null;
   const dispatch = useDispatch(); // 디스패치 함수를 가져옵니다.
 
   const closeModal = () => {
     if (btn === '메뉴') {
-      setTitle('');
+      reset();
       setIsOpen(false);
     } else {
-      setTitle('');
-      setLink('');
-      setWindow(0);
+      reset();
       setIsOpen(false);
     }
   };
@@ -52,12 +53,14 @@ const Modal = ({ isOpen, setIsOpen }) => {
         }
         alert('메뉴를 추가하였습니다.');
         setIsOpen(false);
-        setTitle('');
+        reset();
         dispatch(updateFirstList([...firstList, data]));
       } catch (err) {
         console.error(err);
       }
     };
+
+  
 
     const saveMenu = async () => {
       if (title === '' || link === '') {
@@ -69,7 +72,7 @@ const Modal = ({ isOpen, setIsOpen }) => {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           credentials: 'include',
-          body: JSON.stringify({ title, link, parent_id, new_window: window }),
+          body: JSON.stringify({ title, link, parent_id, new_window }),
         });
 
         const data = await res.json();
@@ -80,9 +83,7 @@ const Modal = ({ isOpen, setIsOpen }) => {
         }
         alert('메뉴를 추가하였습니다.');
         setIsOpen(false);
-        setTitle('');
-        setLink('');
-        setWindow(0);
+        reset();
         dispatch(updateSecondList([...secondList, data]));
       } catch (err) {
         console.error(err);
@@ -103,13 +104,13 @@ const Modal = ({ isOpen, setIsOpen }) => {
           <div className='modal-content' onClick={handleModalContentClick}>
             <div className='modal_infor_box'>
               <span>{btn !== '메뉴' && btn !== '복제' ? '메뉴 항목 추가' : `페이지 ${btn}`}</span>
-              <input type='text' className='pageInput' name='title' placeholder={btn === '메뉴' ? '메뉴 항목' : `페이지 명`} value={title} onChange={(e) => setTitle(e.target.value)} />
-              {btn !== '메뉴' && <input type='text' className='pageInput' name='link' placeholder='페이지 주소' value={link} onChange={(e) => setLink(e.target.value)} />}
+              <input type='text' className='pageInput' name='title' placeholder={btn === '메뉴' ? '메뉴 항목' : `페이지 명`} value={title} onChange={handleChange} />
+              {btn !== '메뉴' && <input type='text' className='pageInput' name='link' placeholder='페이지 주소' value={link} onChange={handleChange} />}
               {btn !== '메뉴' && (
                 <div className='modal_page_infor'>
                   <p>{`http://localhost:3000/page/${link}`}</p>
                   <div>
-                    <input type='checkbox' checked={window === 1} onChange={() => setWindow(window === 0 ? 1 : 0)} />
+                    <input type='checkbox' name='new_window' checked={new_window} onChange={handleChange} />
                     새창 열기
                   </div>
                 </div>
@@ -127,3 +128,4 @@ const Modal = ({ isOpen, setIsOpen }) => {
 };
 
 export default Modal;
+
---
+++ client/src/hooks/useInput.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+const useInput = (initialState) => {
+  const [inputValues, setInputValues] = useState(initialState);
+
+  const handleChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    const newValue = type === 'checkbox' ? (checked ? 1 : 0) : value;
+
+    setInputValues((prevInputValues) => ({
+      ...prevInputValues,
+      [name]: newValue,
+    }));
+  };
+
+  const reset = () => {
+    setInputValues(initialState);
+  };
+
+  return {
+    inputValues,
+    handleChange,
+    reset,
+  };
+};
+
+export default useInput;
